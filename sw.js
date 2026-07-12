0f6809408a13568bc313eda5ea55ba4af5f9652b
firres190 <125687302+firres190@users.noreply.github.com>
2026-07-12 10:53:35 -0500

    new

--- sw.js
+++
@@ -1,5 +0,0 @@
-// Service worker mínimo: no cachea nada, solo permite que el navegador
-// considere el sitio "instalable" en el celular.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {});
